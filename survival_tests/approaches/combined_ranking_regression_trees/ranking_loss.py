b3d586b519605dad46b710194b27db11c48d04b8
Lukas Fehring <fehring2@mail.upb.de>
2022-02-09 15:00:26 +0100

    rework modified position error

--- survival_tests/approaches/combined_ranking_regression_trees/ranking_loss.py
+++ survival_tests/approaches/combined_ranking_regression_trees/ranking_loss.py
@@ -7,8 +7,8 @@ def modified_position_error(performance_data, borda_score, rankings):
     consensus_ranking = borda_score(rankings)
     avg_performances = np.mean(performance_data, axis=0)
     error = 0
-    for ranking in rankings:
-        val = avg_performances[consensus_ranking[0]] - avg_performances[ranking[0]]  # todo how to handle tied values in ranking. ranking[0] could be 6.5 if there is a tie
+    for ranking in rankings:  # todo this is not efficient
+        val = avg_performances[int(consensus_ranking[0])] - avg_performances[int(ranking[0])]
         error += val
     return error / len(rankings)
 
