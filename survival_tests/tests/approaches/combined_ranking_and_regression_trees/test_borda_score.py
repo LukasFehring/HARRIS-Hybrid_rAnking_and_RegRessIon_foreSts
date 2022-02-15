e68f86fdf42bf18424ebe2dd54067642884fed1d
Lukas Fehring <fehring2@mail.upb.de>
2022-02-15 10:48:09 +0100

    updatze borda score to start

--- survival_tests/tests/approaches/combined_ranking_and_regression_trees/test_borda_score.py
+++ survival_tests/tests/approaches/combined_ranking_and_regression_trees/test_borda_score.py
@@ -20,6 +20,6 @@ class TestRankingTransformer(unittest.TestCase):
         borda_ranking = borda_score_mean(self.ranking_data)
         self.assertTrue(
             np.array_equal(
-                borda_ranking, [23.0, 24.0, 18.0, 5.0, 4.0, 3.0, 6.0, 13.0, 7.0, 15.0, 11.0, 27.0, 27.0, 8.0, 20.0, 19.0, 22.0, 21.0, 2.0, 1.0, 17.0, 14.0, 9.5, 9.5, 12.0, 16.0, 27.0, 27.0, 27.0]
+                borda_ranking, [22.0, 23.0, 17.0, 4.0, 3.0, 2.0, 5.0, 12.0, 6.0, 14.0, 10.0, 28.0, 28.0, 7.0, 19.0, 18.0, 21.0, 20.0, 1.0, 0.0, 16.0, 13.0, 8.5, 8.5, 11.0, 15.0, 28.0, 28.0, 28.0]
             )
         )
