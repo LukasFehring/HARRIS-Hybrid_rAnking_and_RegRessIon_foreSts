c8df049c74d501e79b837a490c72bb1bda476c39
Lukas <fehring2@mail.upb.de>
2022-04-13 16:38:43 +0200

    update_data_visualisation

--- survival_tests/fetch_dataframe_from_server.py
+++ survival_tests/fetch_dataframe_from_server.py
@@ -1,6 +1,8 @@
 import configparser
 
+import fontTools
 import pandas as pd
+from cycler import cycler
 from matplotlib import pyplot as plt
 
 import database_utils
@@ -26,14 +28,16 @@ def plot_data(sql_statement, x_axis_column, y_axis_column, label, title, sbs_sql
     df_sbs = fetch_dataframe_from_server(sbs_sql_statement)
     df_per_algo = fetch_dataframe_from_server(per_algorithm_regressor_sql_statement)
     fig, axs = plt.subplots()
-
+    axs.set_prop_cycle(cycler(color=["c", "m", "y", "k"]))
+    fig.set_size_inches(25, 10, forward=True)
     axs.plot(df["impact_factor"], df["result"], "x", label=label)
 
-    axs.axhline(y=df_sbs["result"].iat[0], color="r", linestyle="--", label="regression forest baseline")
-    axs.axhline(y=df_per_algo["result"].iat[0], color="g", linestyle="--", label="single best folder baseline")
+    axs.axhline(y=df_sbs["result"].iat[0], linestyle="--", label="regression forest")
+    axs.axhline(y=df_per_algo["result"].iat[0], linestyle="--", label="single best solver")
 
-    fig.suptitle(title, fontsize=16)
-    axs.set_xlabel(x_axis_column)
-    axs.set_ylabel(y_axis_column)
+    fig.suptitle(title, fontsize=40)
+    axs.set_xlabel(x_axis_column, fontsize=30)
+    axs.set_ylabel(y_axis_column, fontsize=30)
 
-    plt.legend(loc=(0.2, -0.4))
+    plt.legend(loc=(0.5, -0.4), fontsize=20)
+    plt.savefig(title + ".png")
