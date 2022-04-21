7470c66936f8ab5e458159e448556a98f060d776
Lukas Fehring <fehring2@mail.upb.de>
2022-04-21 14:50:25 +0200

    update stopping_criteria

--- survival_tests/approaches/combined_ranking_regression_trees/stopping_criteria.py
+++ survival_tests/approaches/combined_ranking_regression_trees/stopping_criteria.py
@@ -31,7 +31,7 @@ def loss_under_threshold(
     return _calc_loss() <= threshold, _calc_loss()
 
 
-def same_ranking(performance_data: np.array, min_sample_split, impact_factor, depth=None, threshold=None, old_threshold=None):
+def same_ranking(performance_data: np.array, min_sample_split, impact_factor, depth=None, threshold=None, old_threshold=None, borda_score=None, ranking_loss=None):
     if min_sample_split is not None and np.ma.size(performance_data, axis=0) < min_sample_split:
         return True, None
 
@@ -46,7 +46,7 @@ def same_ranking(performance_data: np.array, min_sample_split, impact_factor, de
     return True, None
 
 
-def same_ranking_percentage(performance_data: np.array, min_sample_split, impact_factor, depth=None, percentage=0.75, threshold=None, old_threshold=None):
+def same_ranking_percentage(performance_data: np.array, min_sample_split, impact_factor, depth=None, percentage=0.75, threshold=None, old_threshold=None, borda_score=None, ranking_loss=None):
     if min_sample_split is not None and np.ma.size(performance_data, axis=0) < min_sample_split:
         return True, None
 
@@ -58,7 +58,7 @@ def same_ranking_percentage(performance_data: np.array, min_sample_split, impact
     for i in range(math.ceil((1 - percentage) * len(performance_data))):
         same = list()
         for column in np.transpose(rankings):
-            if np.all(column == column[0]): 
+            if np.all(column == column[0]):
                 same.append(1)
             else:
                 same.append(0)
@@ -66,7 +66,7 @@ def same_ranking_percentage(performance_data: np.array, min_sample_split, impact
             return True, None
 
 
-def max_depth(performance_data: np.array, min_sample_split, impact_factor, depth=None, threshold=65, old_threshold=None):
+def max_depth(performance_data: np.array, min_sample_split, impact_factor, depth=None, threshold=65, old_threshold=None, borda_score=None, ranking_loss=None):
     if min_sample_split is not None and np.ma.size(performance_data, axis=0) < min_sample_split:
         return True, None
 
