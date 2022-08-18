6dc829acda39627b95a142abd3241c2831046dd2
Lukas Fehring <fehring2@mail.upb.de>
2022-08-18 17:14:15 +0200

    add debug configuraiton

--- survival_tests/run.py
+++ survival_tests/run.py
@@ -269,12 +269,28 @@ def create_approach(approach_names):
                 forest = Forest(amount_of_trees, binary_decision_tree, consensus=consensus_function, feature_percentage=feature_percentage)
                 approaches.append(forest)
 
+        if approach_name == "debug_forests":
+            ranking_loss = copy.deepcopy(modified_position_error)
+            regression_loss = copy.deepcopy(mean_square_error)
+            borda_score = borda_score_mean_ranking
+            impact_factor = 0.5
+            stopping_criterion = max_depth
+            stopping_depth = 1
+            amount_of_trees = 3
+            feature_percentage = 0.7
+            consensus_function = average_runtimes
+            stopping_criterion = max_depth
+            binary_decision_tree = BinaryDecisionTree(ranking_loss, regression_loss, borda_score, impact_factor, stopping_criterion, stopping_threshold=stopping_depth)
+            forest = Forest(amount_of_trees, binary_decision_tree, consensus=consensus_function, feature_percentage=feature_percentage)
+            approaches.append(forest)
+
         if approach_name == "evaluate_consensus_function":
             ranking_loss = copy.deepcopy(modified_position_error)
             regression_loss = copy.deepcopy(mean_square_error)
             borda_score = borda_score_mean_ranking
             impact_factor = 0.5
             stopping_criterion = max_depth
+            stopping_depth = 3
             amount_of_trees = 30
             feature_percentage = 0.7
             for consensus_function in (average_runtimes, max_runtimes, min_runtimes):
